74885b0fb9bca4bef9d40dc91bac7b7cfe9b02e7
robinfransson <71066647+robinfransson@users.noreply.github.com>
2021-02-17 22:31:37 +0100

    Förenklade koden i Index och Item
    Använder HttpClient istället för att hämta json från APIt

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Net.Http;
 
 namespace Bakdelar.Pages
 {
@@ -22,37 +24,21 @@ namespace Bakdelar.Pages
         }
         public void OnGet()
         {
+            //länken till ProductController i APIt
+            string productsApiLink = "https://localhost:44347/api/Products";
+
+            using var httpClient = new HttpClient();
+
+            //hämtar listan med produkter ifrån apit
+            Products = httpClient.GetFromJsonAsync<List<Classes.Product>>(productsApiLink).Result;
 
 
-            var jsonText = GetProductInfo("https://localhost:44347/api/Products?adminToken=dXNlcmlzYWRtaW5zaG93ZnVsbHByb2R1Y3Q=");
-            if (jsonText != string.Empty)
-            {
-                var options = new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                };
 
-                Products = JsonSerializer.Deserialize<List<Classes.Product>>(jsonText, options);
-            }
-            else
+            //är det inga produkter i listan är det något fel
+            if (!Products.Any())
             {
                 Error = true;
             }
-        }
-
-        string GetProductInfo(string jsonDataURL)
-        {
-            string jsonText = "";
-            using var webClient = new WebClient();
-            try
-            {
-                jsonText = webClient.DownloadString(jsonDataURL);
-            }
-            catch (Exception e)
-            {
-                Redirect("~/Error");
-            }
-            return jsonText;
         }
     }
 }
